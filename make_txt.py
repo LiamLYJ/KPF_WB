61d84671ea3ac8d0689aa678ed6630aaf8a78439
yongjie.liu <yongjie.liu@dji.com>
2018-08-17 11:47:35 +0900

    update dataset

---
+++ make_txt.py
@@ -0,0 +1,20 @@
+import os
+
+folder = '/Users/lyj/Desktop/check_rggb/check_0'
+out_file = './tmp.txt'
+
+with open(out_file, 'w') as out_file:
+    for root, dirs, files in os.walk(folder):
+        # print (root)
+        # print (dirs)
+        # print (files)
+        for file in files:
+            if file.endswith('txt'):
+                count = 0
+                with open(os.path.join(root, file), 'r') as fp:
+                    for line in fp:
+                        if count %3 == 0:
+                            out_file.write(line)
+                        elif count %3 == 1:
+                            out_file.write(line[:-2] + '\n')
+                        count += 1
