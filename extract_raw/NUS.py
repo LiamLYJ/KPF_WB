7963b18c4140e8cd31291a05c23b8bb79f556f06
yongjie.liu <yongjie.liu@dji.com>
2018-08-28 20:49:30 +0900

    updated with nus

--- extract_raw/NUS.py
+++ extract_raw/NUS.py
@@ -3,6 +3,7 @@ from glob import glob
 import cv2
 import scipy.io
 import numpy as np
+import random
 
 
 def load_image(fn, darkness_level, saturation_level):
@@ -29,7 +30,7 @@ def check_NUS(data_path, id = None):
     darkness_level = ground_truth['darkness_level']
     saturation_level = ground_truth['saturation_level']
     illums /= np.linalg.norm(illums, axis=1)[..., np.newaxis]
-    filenames = glob(os.path.join(data_path, '*.PNG'))
+    filenames = glob(os.path.join(data_path, camera_name, '*.PNG'))
     filenames = sorted(filenames)
     filenames = list(filter(lambda f: f.split('/')[-1].startswith(camera_name), filenames))
     extras = {
@@ -50,6 +51,7 @@ def check_NUS(data_path, id = None):
         img12 = (processed_raw / (2**16 -1)) * 100.0
 
         image = convert_to_8bit(img12, 2.5)
+        image = cv2.resize(image, (512,512))
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
         image[:,:,0] = np.minimum(image[:,:,0] * Gain_R, 255)
@@ -73,72 +75,96 @@ def check_NUS(data_path, id = None):
         cv2.waitKey()
 
 
-def make_txt_file(data_path, save_dir = None, id = 0):
+def make_txt_file(data_path, save_dir = None):
     camera_names = [
         'Canon1DsMkIII', 'Canon600D', 'FujifilmXM1', 'NikonD5200',
         'OlympusEPL6', 'PanasonicGX1', 'SamsungNX2000', 'SonyA57'
     ]
+    bad_count = 0
+    for id in range(8):
+        camera_name = camera_names[id]
+        ground_truth = scipy.io.loadmat(os.path.join(data_path,
+                                        camera_name + '_gt.mat'))
+        illums = ground_truth['groundtruth_illuminants']
+        darkness_level = ground_truth['darkness_level']
+        saturation_level = ground_truth['saturation_level']
+        illums /= np.linalg.norm(illums, axis=1)[..., np.newaxis]
+        filenames = glob(os.path.join(data_path, camera_name, '*.PNG'))
+        filenames = sorted(filenames)
+        filenames = list(filter(lambda f: f.split('/')[-1].startswith(camera_name), filenames))
+        extras = {
+            'darkness_level': darkness_level,
+            'saturation_level': saturation_level
+        }
+        count = 0
 
-    camera_name = camera_names[id]
-    ground_truth = scipy.io.loadmat(os.path.join(data_path,
-                                    camera_name + '_gt.mat'))
-    illums = ground_truth['groundtruth_illuminants']
-    darkness_level = ground_truth['darkness_level']
-    saturation_level = ground_truth['saturation_level']
-    illums /= np.linalg.norm(illums, axis=1)[..., np.newaxis]
-    filenames = glob(os.path.join(data_path, '*.PNG'))
-    filenames = sorted(filenames)
-    filenames = list(filter(lambda f: f.split('/')[-1].startswith(camera_name), filenames))
-    extras = {
-        'darkness_level': darkness_level,
-        'saturation_level': saturation_level
-    }
-    count = 0
+        filename_train = './data_txt_file/NUS_train_%s.txt'%(camera_name)
+        filename_val = './data_txt_file/NUS_val_%s.txt'%(camera_name)
+        all_num = len(filenames)
+        val_num = int( 1/ 7 * all_num)
+        with open(filename_val, 'w') as val_file:
+            with open(filename_train, 'w') as train_file:
+                for i in range(len(filenames)):
+                    fn = filenames[i]
+                    illum = illums[i]
 
-    train_file = './data_txt_file/NUS_train.txt'
-    val_file = './data_txt_file/NUS_val.txt'
-    val_num = 50
-    for i in range(len(filenames)):
-        fn = filenames[i]
-        illum = illums[i]
+                    Gain_R = illum[1] / illum[0]
+                    Gain_G = illum[1] / illum[1]
+                    Gain_B = illum[1] / illum[2]
 
-        Gain_R = illum[1] / illum[0]
-        Gain_G = illum[1] / illum[1]
-        Gain_B = illum[1] / illum[2]
+                    try:
+                        processed_raw = load_image(fn, extras['darkness_level'], extras['saturation_level'])
+                    except:
+                        print ('fn is:', fn)
+                        bad_count += 1
+                        print ('bad image found, skipping')
+                        continue
 
-        processed_raw = load_image(fn, extras['darkness_level'], extras['saturation_level'])
+                    img8 = (np.clip(processed_raw / processed_raw.max(), 0, 1) * 255.0).astype(np.uint8)
+                    image = cv2.resize(img8, (512,512))
 
-        img8 = (np.clip(processed_raw / processed_raw.max(), 0, 1) * 255.0).astype(np.uint8)
-        imgae = cv2.imresize(image, (512,512))
+                    if save_dir is not None:
+                        save_name = os.path.join(save_dir,fn.split('/')[-1])
+                        cv2.imwrite(save_name, image)
 
-        if save_dir is not None:
-            save_name = os.path.join(save_dir,item_name.split('/')[-1])
-            cv2.imwrite(save_name, image)
+                    if count < val_num and (random.random() > 0.6):
+                        count += 1
+                        write_file = val_file
+                    else:
+                        write_file = train_file
+                    write_file.write(fn.split('/')[-1] + '\n')
+                    write_file.write(str(Gain_R) + ',' + str(Gain_G) + ',' + str(Gain_B) + '\n')
 
-        if count < val_num:
-            count += 1
-            write_file = val_file
-        else:
-            write_file = train_file
-        write_file.write(item_name.split('/')[-1] + '\n')
-        write_file.write(str(Gain_R) + ',' + str(Gain_G) + ',' + str(Gain_B) + '\n')
-        # print ('item_name:', item_name)
-        # print ('index: ', index)
-        # print ('Gain_R:', Gain_R)
-        # print ('Gain_G:', Gain_G)
-        # print ('Gain_B:', Gain_B)
+                    # print ('index: ', index + 1)
+                    print ('img_name : ', fn)
+                    print ('Gain_R:', Gain_R)
+                    print ('Gain_G:', Gain_G)
+                    print ('Gain_B:', Gain_B)
 
-        # print ('index: ', index + 1)
-        print ('img_name : ', fn)
-        print ('Gain_R:', Gain_R)
-        print ('Gain_G:', Gain_G)
-        print ('Gain_B:', Gain_B)
-        #
-        # cv2.imshow('12bit', img12)
-        cv2.imshow('8bit', img8)
+    print ('bad_count: ', bad_count)
 
-        cv2.waitKey()
+def process_nux_txt(path):
+    filename_train = './data_txt_file/NUS_train.txt'
+    filename_val = './data_txt_file/NUS_val.txt'
+    all_train_txt = glob(os.path.join(path, 'NUS_train*'))
+    all_val_txt = glob(os.path.join(path, 'NUS_val*'))
+    with open(filename_train, 'w') as output:
+        for item in all_train_txt:
+            with open(item, 'r') as input:
+                content_lines = input.readlines()
+                for line in content_lines:
+                    output.write(line)
+    with open(filename_val, 'w') as output:
+        for item in all_val_txt:
+            with open(item, 'r') as input:
+                content_lines = input.readlines()
+                for line in content_lines:
+                    output.write(line)
+
 
 if __name__ == '__main__':
-    data_path = '/Users/lyj/Desktop/Canon_PNG'
-    check_NUS(data_path, id=0)
+    data_path = '/Users/lyj/Documents/AWB_data/nus/'
+    # check_NUS(data_path, id=0)
+    # make_txt_file(data_path, save_dir = '/Users/lyj/Desktop/nus')
+    path = './data_txt_file'
+    process_nux_txt(path)
